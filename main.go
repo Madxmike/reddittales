ac44a7a6f47f74f5b5443cc2e5ec3aa4d5c65171
Michael <mep053@gmail.com>
2020-01-02 03:33:14 -0500

    Add title, dont request if mp3 already exists

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ const (
 )
 
 type TextData struct {
+	Title   string `json:"title"`
 	Text    string `json:"text"`
 	Speaker string `json:"speaker"`
 	Style   string `json:"style"`
@@ -31,47 +32,69 @@ func main() {
 	}
 
 	for name, d := range data {
+		trimmedName := strings.Trim(name, ".json")
+		if VoiceClipExists(PATH_VOICE_CLIPS, trimmedName) {
+			log.Printf("Skipping %s as it already exists!", trimmedName)
+			continue
+		}
 		b, err := GetVoiceClip(http.DefaultClient, d)
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = SaveVoiceFile(PATH_VOICE_CLIPS, strings.Trim(name, ".json"), b)
+		err = SaveVoiceFile(PATH_VOICE_CLIPS, trimmedName, b)
 		log.Println(err)
 	}
 }
 
+func VoiceClipExists(path string, name string) bool {
+	_, err := os.Open(path + name + ".mp3")
+	return err == nil
+}
+
 func GetVoiceClip(client *http.Client, data TextData) ([]byte, error) {
+
+	bytes := make([]byte, 0)
+	b, err := ProcessVoiceRequest(client, data.Title, data.Speaker, data.Style, data.SSML)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not process title")
+	}
+	bytes = append(bytes, b...)
+	for _, text := range SplitText(data.Text) {
+		b, err = ProcessVoiceRequest(client, text, data.Speaker, data.Style, data.SSML)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not process text")
+		}
+		bytes = append(bytes, b...)
+	}
+
+	return bytes, nil
+}
+
+func ProcessVoiceRequest(client *http.Client, text, speaker, style, ssml string) ([]byte, error) {
 	req, err := http.NewRequest("GET", API_ENDPOINT, nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to create request")
 	}
 
 	query := req.URL.Query()
-	query.Add("text", "text")
-	query.Add("speaker", data.Speaker)
-	query.Add("style", data.Style)
-	query.Add("ssml", data.SSML)
-	bytes := make([]byte, 0)
-	for _, text := range SplitText(data.Text) {
-		query.Set("text", text)
-		req.URL.RawQuery = query.Encode()
-		log.Printf("Requesting %s", req.URL.RequestURI())
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to request voice clip\n")
-		}
-		log.Println(resp.Status)
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to read response")
-		}
-		if resp.StatusCode != http.StatusOK {
-			log.Printf("could not get voice clip for %s", text)
-		}
-		bytes = append(bytes, b...)
+	query.Add("text", text)
+	query.Add("speaker", speaker)
+	query.Add("style", style)
+	query.Add("ssml", ssml)
+	req.URL.RawQuery = query.Encode()
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to request voice clip\n")
 	}
-
-	return bytes, nil
+	log.Println(resp.Status)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read response")
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("could not get voice clip")
+	}
+	return b, nil
 }
 
 func SplitText(text string) []string {
