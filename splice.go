082ffd931bd4292a5edf8e8c51a08e46f7e322e8
Michael <mep053@gmail.com>
2020-01-05 06:37:48 -0500

    Stitch the file upward recursively

--- splice.go
+++ splice.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type Splicer struct {
@@ -21,55 +22,90 @@ func (s *Splicer) Start(ctx context.Context) {
 	for {
 		select {
 		case in := <-s.Input:
-			err := s.process(in)
+			processedFilenames, err := s.process(in)
 			if err != nil {
 				log.Println(err)
+				continue
 			}
+			finalFilename, err := s.stitchVideo(s.outputPath, s.reverseNames(processedFilenames))
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Println(finalFilename)
 		case <-ctx.Done():
 			return
 		}
 	}
 }
 
-func (s *Splicer) process(data Data) error {
+func (s *Splicer) process(data Data) ([]string, error) {
+	processedFilenames := make([]string, 0)
 	dirName := fmt.Sprintf("%s%s/", s.outputPath, data.ID)
 	_ = os.Mkdir(dirName, os.ModeDir)
 	for _, comment := range data.Comments {
 		comment.ID = fmt.Sprintf("%s/%s", data.ID, comment.ID)
-		err := s.process(comment)
+		processed, err := s.process(comment)
 		if err != nil {
-			return errors.Wrap(err, "could not process comment")
+			return nil, errors.Wrap(err, "could not process comment")
 		}
+		processedFilenames = append(processedFilenames, processed...)
 	}
 
-	outputFilename, err := s.stitchAV(data)
+	outputFilenames, err := s.stitchAV(data)
 	if err != nil {
-		return errors.Wrap(err, "could not process data")
+		return nil, errors.Wrap(err, "could not process data")
 	}
 
-	log.Println(outputFilename)
+	processed, err := s.stitchVideo(dirName, outputFilenames)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not stitch video")
+	}
 
-	return nil
+	return append(processedFilenames, processed), nil
 }
 
-func (s *Splicer) stitchAV(data Data) (string, error) {
-	var outputFilename string
+func (s *Splicer) stitchVideo(dirName string, filenames []string) (string, error) {
+	var processedFilename string
+	b := make([]byte, 0)
+	for _, name := range filenames {
+		write := []byte(fmt.Sprintf("file '%s'\n", name))
+		b = append(b, write...)
+	}
+	muxFilename := fmt.Sprintf("%s%s", dirName, "filenames.txt")
+	err := ioutil.WriteFile(muxFilename, b, 0777)
+	if err != nil {
+		return processedFilename, errors.Wrap(err, "could not write filenames file")
+	}
+	processedFilename = dirName + "output.mkv"
+	err = s.execute("-f", "concat", "-safe", "0", "-i", muxFilename, "-c", "copy", processedFilename)
+	if err != nil {
+		return processedFilename, errors.Wrap(err, "could not combine files")
+	}
+
+	return strings.TrimPrefix(processedFilename, s.outputPath), nil
+}
+
+func (s *Splicer) stitchAV(data Data) ([]string, error) {
 	lines := data.Lines()
+	outputFilenames := make([]string, 0, len(lines))
+
 	if data.Title != "" {
 		lines = append([]string{data.Title}, lines...)
 	}
 
 	for k := range lines {
-		outputFilename = fmt.Sprintf("%d.mkv", k)
+		outputFilename := fmt.Sprintf("%d.mkv", k)
 		screenshotFilename := fmt.Sprintf("%s%d.png", s.screenshotDir(data), k)
 		voiceclipFilename := fmt.Sprintf("%s%d.mp3", s.voiceDir(data), k)
 		stitchedFilename := fmt.Sprintf("%s%s", s.outputDir(data), outputFilename)
 		err := s.executeStitch(screenshotFilename, voiceclipFilename, stitchedFilename)
 		if err != nil {
-			return outputFilename, errors.Wrap(err, "could not execute stitch")
+			return nil, errors.Wrap(err, "could not execute stitch")
 		}
+		outputFilenames = append(outputFilenames, outputFilename)
 	}
-	return outputFilename, nil
+	return outputFilenames, nil
 }
 
 func (s *Splicer) executeStitch(screenshotFilename, voiceClipFilename, stitchFilename string) error {
@@ -78,54 +114,6 @@ func (s *Splicer) executeStitch(screenshotFilename, voiceClipFilename, stitchFil
 		return errors.Wrap(err, "could not splice files")
 	}
 	return nil
-}
-
-func (s *Splicer) combine(path string, fileNames []string) error {
-	outputFileName, err := s.writeFileNames(path, fileNames)
-	if err != nil {
-		return errors.Wrap(err, "could not combine files")
-	}
-	err = s.execute("-f", "concat", "-safe", "0", "-i", outputFileName, "-c", "copy", path+"output.mkv")
-	if err != nil {
-		return errors.Wrap(err, "could not combine files")
-	}
-	return nil
-}
-
-func (s *Splicer) combineFinal(data Data) error {
-	outfilesName := "output.mkv"
-	basePath := fmt.Sprintf("%s%s/", s.outputPath, data.ID)
-	fileNames := []string{outfilesName}
-	for k := range data.Comments {
-		fileNames = append(fileNames, fmt.Sprintf("%d/%s", k, outfilesName))
-	}
-	outputFileName, err := s.writeFileNames(basePath, fileNames)
-	if err != nil {
-		return errors.Wrap(err, "could not combine files")
-	}
-
-	err = s.execute("-f", "concat", "-safe", "0", "-i", outputFileName, "-c", "copy", basePath+"finished.mkv")
-	if err != nil {
-		return errors.Wrap(err, "could not combine files")
-	}
-	return nil
-}
-
-func (s *Splicer) writeFileNames(path string, fileNames []string) (string, error) {
-	if len(fileNames) == 0 {
-		return "", nil
-	}
-	outputFileName := path + "filenames.txt"
-	b := make([]byte, 0)
-	for _, name := range fileNames {
-		write := fmt.Sprintf("file '%s'\n", name)
-		b = append(b, []byte(write)...)
-	}
-	err := ioutil.WriteFile(outputFileName, b, 0777)
-	if err != nil {
-		return "", errors.Wrap(err, "could not write file names")
-	}
-	return outputFileName, nil
 }
 
 func (s *Splicer) execute(args ...string) error {
@@ -139,6 +127,14 @@ func (s *Splicer) execute(args ...string) error {
 	}
 
 	return nil
+}
+
+func (s *Splicer) reverseNames(filenames []string) []string {
+	for i, j := 0, len(filenames)-1; i < j; i, j = i+1, j-1 {
+		filenames[i], filenames[j] = filenames[j], filenames[i]
+	}
+
+	return filenames
 }
 
 func (s *Splicer) cleanup(dir string) error {
