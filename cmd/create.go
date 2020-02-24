2dc665b2080031f3f30a0247a397c5fc4f0a0a76
Michael Epps <mep053@gmail.com>
2020-02-24 15:25:35 -0500

    implement remaining flags and output

--- cmd/create.go
+++ cmd/create.go
@@ -3,11 +3,16 @@ package cmd
 import (
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	"context"
+	"fmt"
 	"github.com/madxmike/reddittales/internal"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/turnage/graw/reddit"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"sync"
 )
 
@@ -15,10 +20,15 @@ func CreateCmd() *cobra.Command {
 	var sort string
 	var time string
 	var num int
+	var outputPath string
+	var outputFiletype string
+	var maximumNumComments int
+	var filterDistiguishedComments bool
+	var commentKarmaThreshold int
 	create := &cobra.Command{
 		Use:   "create [subreddit name]",
 		Short: "creates a video from the specified parameters",
-		Args:  cobra.MinimumNArgs(1),
+		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			rw, err := internal.NewRedditWorker(agentFile)
 			if err != nil {
@@ -43,8 +53,15 @@ func CreateCmd() *cobra.Command {
 			audioGenerator := internal.AudioGenerator{
 				Client: ttsClient,
 			}
+			filters := []func(comment *reddit.Comment) bool{
+				internal.FilterKarma(int32(commentKarmaThreshold)),
+			}
+			if filterDistiguishedComments {
+				filters = append(filters, internal.FilterDistinguished)
+			}
+
 			for _, p := range posts {
-				comments, err := rw.GetComments(p, 5)
+				comments, err := rw.GetComments(p, maximumNumComments, filters...)
 				if err != nil {
 					log.Println(errors.Wrap(err, "could not retrieve post comments"))
 					continue
@@ -65,14 +82,28 @@ func CreateCmd() *cobra.Command {
 				wg.Wait()
 				close(finished)
 			}()
+			fileNum := 0
 			for data := range finished {
-				log.Println(len(data))
+				fileName := fmt.Sprintf("output_%d.%s", fileNum, outputFiletype)
+				if outputPath != "" {
+					outputPath = strings.TrimSuffix(outputPath, string(os.PathSeparator))
+					fileName = fmt.Sprintf("%s%c%s", outputPath, os.PathSeparator, fileName)
+				}
+				err = ioutil.WriteFile(fileName, data, os.ModePerm)
+				if err != nil {
+					log.Println(errors.Wrap(err, "could not write output"))
+					continue
+				}
 			}
 		},
 	}
 	create.Flags().StringVar(&sort, "sort", "top", "how to sort subreddit posts")
 	create.Flags().StringVar(&time, "time", "day", "filter subreddit posts by time")
 	create.Flags().IntVar(&num, "num", 1, "how many posts should be retrieved")
-
+	create.Flags().StringVar(&outputPath, "outputPath", "", "the output directory of created files")
+	create.Flags().StringVar(&outputFiletype, "filetype", "mkv", "the file type of the finished file")
+	create.Flags().IntVar(&maximumNumComments, "maximumNumComments", 10, "the maximum number of comments included")
+	create.Flags().BoolVar(&filterDistiguishedComments, "filterDistinguished", true, "filter distinguished comments i.e. automod")
+	create.Flags().IntVar(&commentKarmaThreshold, "commentKarmaThreshold", 1000, "the minimum karma required for a comment to be included")
 	return create
 }
